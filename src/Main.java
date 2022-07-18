2b69aa333f7ac340fae32e66688fd545bbe1dc02
Sergey Kubetskiy <kubetskiy@gmail.com>
2022-07-18 23:40:39 +0300

    branch stat: Added TrimmedMean method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         var sm = new SalesManager(new int[]{66, 22, 11, 33, 44});
-        System.out.printf("Max of {66, 22, 11, 33, 44} = %d", sm.max());
+        System.out.printf("Max of {66, 22, 11, 33, 44} = %d\n", sm.max());
+        System.out.printf("TrimmedMean of {66, 22, 11, 33, 44} = %d\n", sm.trimmedMean());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,4 +1,7 @@
-public class SalesManager {
+/**
+ * TODO Прудполагается, что sales.length >= 3
+ */
+    public class SalesManager {
     protected int[] sales;
 
     public SalesManager(int[] sales) {
@@ -13,5 +16,22 @@ public class SalesManager {
             }
         }
         return max;
+    }
+    public int trimmedMean() {
+        int max = -1, tm, sum = 0;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+            sum += sale;
+        }
+        int min = max;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        tm = (sum - (min + max))/ (sales.length - 2);
+        return tm;
     }
 }
